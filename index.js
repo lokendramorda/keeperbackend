36ab4c44c2b56513740b24b99eecea5e5d3ed419
loki <31621774+lokendramorda@users.noreply.github.com>
2023-08-04 17:21:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ global.formData = require('./db')(function call(err, data) {
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   
   app.use((req, res, next) => { 
-    res.setHeader("Access-Control-Allow-Origin", "https://keeperhere.netlify.app/");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
